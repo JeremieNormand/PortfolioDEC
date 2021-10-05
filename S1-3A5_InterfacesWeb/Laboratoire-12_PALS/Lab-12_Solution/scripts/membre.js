c838fb3e69f1276430f7721ff813c94872103c4d
Jérémie Normand <jeremie.normand@hotmail.fr>
2021-10-05 12:08:51 -0400

    Edit

--- S1-3A5_InterfacesWeb/Laboratoire-12_PALS/Lab-12_Solution/scripts/membre.js
+++
@@ -1,5 +0,0 @@
-const formulaire = document.querySelector('form');
-
-function generer_numero_de_membre() {
-	formulaire.getElementById('numero-de-membre').value = Math.round(999 * Math.random());
-}
